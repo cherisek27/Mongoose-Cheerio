725ef4353d61f7e8239920c72f04d98ff28b1b4d
Cherise Knox <cheriseknox@Cherises-MacBook-Pro.local>
2017-09-10 14:23:48 -0700

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/heroku_lbkkp8rs:67jpsg31eq4qjsvias6ndbqo4s@ds129394.mlab.com:29394/heroku_lbkkp8rs");
+mongoose.connect("mongodb://heroku_lbkkp8rs:67jpsg31eq4qjsvias6ndbqo4s@ds129394.mlab.com:29394/heroku_lbkkp8rs");
 var db = mongoose.connection;
 
 // Show any mongoose errors
